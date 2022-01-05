a83e26f33541a690a5aa3383bed26fe69389e894
Mathieu Maes <mathieu@zimmo.be>
2022-01-05 20:07:18 +0100

    Add lazy module

---
+++ src/app/lazy/lazy.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lazy',
+  templateUrl: './lazy.component.html',
+  styleUrls: ['./lazy.component.scss']
+})
+export class LazyComponent implements OnInit {
+
+  constructor() {
+    debugger;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
